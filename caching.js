53818fb85bf2f759ed3a79f3b399b25adfd86470
Athithyen <athithyen11@gmail.com>
2022-06-30 21:50:42 -0400

    Datastructures

---
+++ caching.js
@@ -0,0 +1,40 @@
+function multiply(a,b) {
+
+    let product = 0;
+
+    for (i = 0 ; i < b ; i++) {
+      product += a;
+
+
+    }
+
+    return product;
+  }
+
+  console.log(multiply(4,5));
+
+  function recursiveMultiply(a, b) {
+    if (b === 1) return a;
+
+    return a + recursiveMultiply(a, b-1);
+  }
+
+  console.log(recursiveMultiply(4,5));
+  const cache = {};
+
+function multiplyCache(a, b) {
+  // Create a unique key containing the inputs
+  const key = `${a}x${b}`;
+
+  // If the key does not exist in the cache
+  if (cache[key] === undefined) {
+    // run the calculation and store it in the cache
+    cache[key] = multiply(a, b);
+  }
+
+  // Return the cached value
+  return cache[key];
+}
+
+console.log(multiply(2, 1000000000));
+console.log(multiplyCache(2, 1000000000));
